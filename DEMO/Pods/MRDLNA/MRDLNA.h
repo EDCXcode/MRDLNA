98659946616c384e96e6eb1b7e0239dc9f6ca038
付先建 <fuxianjian@yuan9.cn>
2024-08-07 15:00:16 +0800

    添加播放回调

---
+++ DEMO/Pods/MRDLNA/MRDLNA.h
@@ -0,0 +1,112 @@
+//
+//  MRDLNA.h
+//  MRDLNA
+//
+//  Created by MccRee on 2018/5/4.
+//
+
+#import <Foundation/Foundation.h>
+#import "CLUPnP.h"
+#import "CLUPnPDevice.h"
+
+@protocol DLNADelegate <NSObject>
+
+@optional
+/**
+ DLNA局域网搜索设备结果
+ @param devicesArray <CLUPnPDevice *> 搜索到的设备
+ */
+- (void)searchDLNAResult:(NSArray *)devicesArray;
+
+/**
+ 投屏成功开始播放
+ */
+- (void)dlnaStartPlay;
+
+/**
+ 投屏播放进度
+ */
+- (void)dlnaupnpGetPositionInfoResponse:(CLUPnPAVPositionInfo *)info;
+
+
+/**
+ 投屏播放状态
+ */
+- (void)upnpGetTransportInfoResponse:(CLUPnPTransportInfo *)info;
+
+
+@end
+
+@interface MRDLNA : NSObject
+
+@property(nonatomic,weak)id<DLNADelegate> delegate;
+
+@property(nonatomic, strong) CLUPnPDevice *device;
+
+@property(nonatomic,copy) NSString *playUrl;
+
+@property(nonatomic,assign) NSInteger searchTime;
+
+@property (nonatomic,copy) void (^BlockSearch)(BOOL ConnectionStatus);
+
+@property (nonatomic,copy) void (^BlockSocketOff)(void);
+
+/**
+ 单例
+ */
++(instancetype)sharedMRDLNAManager;
+
+/**
+ 搜设备
+ */
+- (void)startSearch;
+
+/**
+ 获取播放进度
+ */
+- (void)GetPositionInfo;
+
+/**
+ DLNA投屏
+ */
+- (void)startDLNA;
+/**
+ DLNA投屏(首先停止)---投屏不了可以使用这个方法
+ ** 【流程: 停止 ->设置代理 ->设置Url -> 播放】
+ */
+- (void)startDLNAAfterStop;
+
+/**
+ 退出DLNA
+ */
+- (void)endDLNA;
+
+/**
+ 播放
+ */
+- (void)dlnaPlay;
+
+/**
+ 暂停
+ */
+- (void)dlnaPause;
+
+/**
+ 设置音量 volume建议传0-100之间字符串
+ */
+- (void)volumeChanged:(NSString *)volume;
+
+/**
+ 设置播放进度 seek单位是秒
+ */
+- (void)seekChanged:(NSInteger)seek;
+
+/**
+ 播放切集
+ */
+- (void)playTheURL:(NSString *)url;
+
+
+
+
+@end
